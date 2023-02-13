65f1f6b3c6ec89eb5a3ac73e602cda072f457b2b
IvanKahinga <ikahinga@gmail.com>
2023-02-13 14:55:14 +0300

    Not yet done

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,4 +1,9 @@
 def starts_with_a_vowel?(word)
+   if word.scan(/\w^[aeiou]/) 
+    return true
+   else 
+    return false
+   end
 
 end
 
@@ -7,6 +12,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
+    text.scan
 
 end
 
